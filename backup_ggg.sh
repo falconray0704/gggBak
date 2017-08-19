f5ac6187ecf44bcf073ab4fcbbbdd49eff34de7b
FalconRay <falconray@yahoo.com>
2017-08-19 09:57:52 +0800

    make backup script

---
+++ backup_ggg.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+
+cd ../
+rm -rf ggg
+cp -a /md/gows/src/github.com/falconray0704/ggg ./
+
+rm -rf ggg/rtsp2rtmp/demos/logs
+rm -rf ggg/rtsp2rtmp/demos/demo
+
+rm -rf ggg/rtsp2rtmp/demos/decVid_rtmp_player/logs
+rm -rf ggg/rtsp2rtmp/demos/decVid_rtmp_player/decVid_rtmp_player
+rm -rf ggg/rtsp2rtmp/demos/decVid_rtmp_player/decVid.h264
+
+rm -rf ggg/rcp/decVid/clt/client
+rm -rf ggg/rcp/decVid/cpp/srv/cam.h264
+rm -rf ggg/rcp/decVid/cpp/srv/decVid_server
+rm -rf ggg/rcp/decVid/cpp/srv/decVid_server.o
+rm -rf ggg/rcp/decVid/cpp/srv/*.pb.h
+rm -rf ggg/rcp/decVid/decVid.pb.go
+rm -rf ggg/rcp/decVid/tags
+rm -rf ggg/rcp/webCfg/clt/client
+rm -rf ggg/rcp/webCfg/clt/client32.exe
+rm -rf ggg/rcp/webCfg/clt/client64.exe
+rm -rf ggg/rcp/webCfg/srv/server
+rm -rf ggg/rcp/webCfg/srv/server32.exe
+rm -rf ggg/rcp/webCfg/srv/server64.exe
+rm -rf ggg/rcp/webCfg/webCfg.pb.go
+
+rm -rf ggg/rcp/webSrv/clt/client
+rm -rf ggg/rcp/webSrv/clt/client32.exe
+rm -rf ggg/rcp/webSrv/clt/client64.exe
+rm -rf ggg/rcp/webSrv/srv/server
+rm -rf ggg/rcp/webSrv/webSrv.pb.go
+
+
+

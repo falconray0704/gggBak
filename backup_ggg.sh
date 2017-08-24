b1eef80d152220a62edb943033b5a00e958885b0
FalconRay <falconray@yahoo.com>
2017-08-24 16:22:10 +0800

    init version with set args grpc api

--- backup_ggg.sh
+++ backup_ggg.sh
@@ -10,6 +10,7 @@ rm -rf ggg/rtsp2rtmp/demos/demo
 rm -rf ggg/rtsp2rtmp/demos/decVid_rtmp_player/logs
 rm -rf ggg/rtsp2rtmp/demos/decVid_rtmp_player/decVid_rtmp_player
 rm -rf ggg/rtsp2rtmp/demos/decVid_rtmp_player/decVid.h264
+rm -rf ggg/rtsp2rtmp/muxers/h264parser/bits/README.md
 
 rm -rf ggg/rcp/decVid/clt/client
 rm -rf ggg/rcp/decVid/cpp/srv/cam.h264
@@ -18,16 +19,20 @@ rm -rf ggg/rcp/decVid/cpp/srv/decVid_server.o
 rm -rf ggg/rcp/decVid/cpp/srv/*.pb.h
 rm -rf ggg/rcp/decVid/cpp/srv/*.pb.o
 rm -rf ggg/rcp/decVid/decVid.pb.go
+rm -rf ggg/rcp/tags
 rm -rf ggg/rcp/decVid/tags
 rm -rf ggg/rcp/webCfg/clt/client
+rm -rf ggg/rcp/webCfg/clt/clientArm
 rm -rf ggg/rcp/webCfg/clt/client32.exe
 rm -rf ggg/rcp/webCfg/clt/client64.exe
 rm -rf ggg/rcp/webCfg/srv/server
+rm -rf ggg/rcp/webCfg/srv/serverArm
 rm -rf ggg/rcp/webCfg/srv/server32.exe
 rm -rf ggg/rcp/webCfg/srv/server64.exe
 rm -rf ggg/rcp/webCfg/webCfg.pb.go
 
 rm -rf ggg/rcp/webSrv/clt/client
+rm -rf ggg/rcp/webSrv/clt/clientArm
 rm -rf ggg/rcp/webSrv/clt/client32.exe
 rm -rf ggg/rcp/webSrv/clt/client64.exe
 rm -rf ggg/rcp/webSrv/srv/server

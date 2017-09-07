e97014c07014a8b2517e4ebdf21dc57ade607e9c
FalconRay <falconray@yahoo.com>
2017-09-07 19:30:33 +0800

    init version with pcClt and ivasAlarm

--- backup_ggg.sh
+++ backup_ggg.sh
@@ -3,12 +3,15 @@
 cd ../
 rm -rf ggg
 cp -a /md/gows/src/github.com/falconray0704/ggg ./
+rm -rf ./ggg/bin
 
 rm -rf ggg/rtsp2rtmp/demos/logs
 rm -rf ggg/rtsp2rtmp/demos/demo
 
 rm -rf ggg/rtsp2rtmp/demos/decVid_rtmp_player/logs
 rm -rf ggg/rtsp2rtmp/demos/decVid_rtmp_player/decVid_rtmp_player
+rm -rf ggg/rtsp2rtmp/demos/decVid_rtmp_player/decVid_rtmp_player32.exe
+rm -rf ggg/rtsp2rtmp/demos/decVid_rtmp_player/decVid_rtmp_player64.exe
 rm -rf ggg/rtsp2rtmp/demos/decVid_rtmp_player/decVid.h264
 rm -rf ggg/rtsp2rtmp/muxers/h264parser/bits/README.md
 
@@ -18,6 +21,7 @@ rm -rf ggg/rcp/decVid/cpp/srv/decVid_server
 rm -rf ggg/rcp/decVid/cpp/srv/decVid_server.o
 rm -rf ggg/rcp/decVid/cpp/srv/*.pb.h
 rm -rf ggg/rcp/decVid/cpp/srv/*.pb.o
+rm -rf ggg/rcp/decVid/cpp/tags
 rm -rf ggg/rcp/decVid/decVid.pb.go
 rm -rf ggg/rcp/tags
 rm -rf ggg/rcp/decVid/tags
@@ -31,15 +35,25 @@ rm -rf ggg/rcp/pcClt/clt/client
 rm -rf ggg/rcp/pcClt/clt/clientArm
 rm -rf ggg/rcp/pcClt/clt/client32.exe
 rm -rf ggg/rcp/pcClt/clt/client64.exe
-rm -rf ggg/rcp/webCfg/clt/client
-rm -rf ggg/rcp/webCfg/clt/clientArm
-rm -rf ggg/rcp/webCfg/clt/client32.exe
-rm -rf ggg/rcp/webCfg/clt/client64.exe
+rm -rf ggg/rcp/webCfg/clt/webCfgClt
+rm -rf ggg/rcp/webCfg/clt/webCfgCltArm
+rm -rf ggg/rcp/webCfg/clt/webCfgClt32.exe
+rm -rf ggg/rcp/webCfg/clt/webCfgClt64.exe
 rm -rf ggg/rcp/webCfg/srv/server
 rm -rf ggg/rcp/webCfg/srv/serverArm
 rm -rf ggg/rcp/webCfg/srv/server32.exe
 rm -rf ggg/rcp/webCfg/srv/server64.exe
 rm -rf ggg/rcp/webCfg/webCfg.pb.go
+
+rm -rf ggg/rcp/ivasAlarm/cpp/clt/ivasAlarm_client
+rm -rf ggg/rcp/ivasAlarm/cpp/clt/ivasAlarm_client.o
+rm -rf ggg/rcp/ivasAlarm/cpp/clt/*.pb.o
+rm -rf ggg/rcp/ivasAlarm/cpp/clt/*.pb.h
+rm -rf ggg/rcp/ivasAlarm/cpp/clt/tags
+rm -rf ggg/rcp/ivasAlarm/srv/ivasAlarmServer
+rm -rf ggg/rcp/ivasAlarm/srv/ivasAlarmServer32.exe
+rm -rf ggg/rcp/ivasAlarm/srv/ivasAlarmServer64.exe
+rm -rf ggg/rcp/ivasAlarm/ivasAlarm.pb.go
 
 rm -rf ggg/rcp/webSrv/clt/client
 rm -rf ggg/rcp/webSrv/clt/clientArm
